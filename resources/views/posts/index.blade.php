dcf308abc67daa410da5d87c694468ef30cc3b64
mikseros <mmadynski@gmail.com>
2022-01-24 14:01:00 +0100

    Authorization

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -31,17 +31,15 @@
                         text-sm">{{ $post->created_at->diffForHumans() }}</span>
 
                         <p class="mb-2">{{ $post->body }}</p>
-
-                        @if ($post->ownedBy(auth()->user()))
-                            <div>
-                                <form action="{{ route('posts.destroy', $post) }}" method="post">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="text-blue-500">Delete</button>
-                                </form>
-                            </div>
-                        @endif
-
+                        
+                        @can('delete', $post)
+                            <form action="{{ route('posts.destroy', $post) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="text-blue-500">Delete</button>
+                            </form>
+                        @endcan
+                        
                         <div class="flex items-center">
                             @auth
                                 @if (!$post->likedBy(auth()->user()))
